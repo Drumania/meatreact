530dd6d3fb086e3238909e3e5e943c6d045c1a58
martinbrumana <martinbrumana@gmail.com>
2020-07-25 20:55:27 -0300

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1>Logo</h1>
+        <h2>
+          El secreto
+          <br />
+          de tu cocina
+        </h2>
+      </div>
+      <ul>
+        <li>
+          <a href="#!" target="_blank">
+            facebook
+          </a>
+        </li>
+        <li>
+          <a href="#!" target="_blank">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a href="#!" target="_blank">
+            Youtube
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bkTitles from "../img/heading-shape.png";
+
+const Title = (title) => {
+  return (
+    <div className="wrap-h3">
+      <h3>${title}</h3>
+      <img src={bkTitles} alt={title} />
+    </div>
+  );
+};
+
+export default Title;
